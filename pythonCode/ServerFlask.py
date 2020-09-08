708a681274105320fb1ccf1d5d64bb7f0ac05555
ShGoldfish <sdavari@vt.edu>
2020-09-08 00:20:38 -0400

    fix detecting 0 faces

--- pythonCode/ServerFlask.py
+++ pythonCode/ServerFlask.py
@@ -21,15 +21,18 @@ def hello_world():
 
 @app.route('/receive-image', methods=['POST'])
 def receive_image():
+	global return_data
 	data = request.files.get('myImage')
 	b = bytes(data.read())
-	print(len(b))
+	# b = bytearray(data)
 	buf = io.BytesIO(b)
 	try:
 		img = Image.open(buf)
 		img.save('frame.jpg')
 	except OSError:
+		return_data = " "
 		print("Cannot save frame")
+	buf.close()
 	image = cv2.imread("frame.jpg")
 	conf = 0.6
 	net = cv2.dnn.readNetFromCaffe("../pythonCode/deploy.prototxt.txt", "../pythonCode/res10_300x300_ssd_iter_140000.caffemodel")
@@ -54,13 +57,13 @@ def receive_image():
 		faces_box = np.append(faces_box,box.astype("int"))#np.around(box, decimals=3))
 	n = faces_box.size
 	if n < 4 :
-		return " "
+		return_data = " "
+		return return_data
 	if n % 4 != 0:
 		faces_box = faces_box[:-n]
 	
-	global return_data
 	return_data = np.array2string(faces_box, separator=',')
-	return "string"
+	return return_data[1:-1]
 
 @app.route('/detect-faces', methods=['GET'])
 def detect_faces():
